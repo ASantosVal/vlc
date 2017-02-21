54727714689d062c27a9293803482865812778c3
Asier Santos <asiersantosval@gmail.com>
2017-02-21 13:59:30 +0100

    Inicio de la lectura de ficheros en reproducción.

--- share/lua/extensions/MiModulo.lua
+++ share/lua/extensions/MiModulo.lua
@@ -1,95 +1,106 @@
 function descriptor()
-  return { title = "StreamIt!",
-           version = "0.1",
-           author = "Asier Santos",
+  return { title = 'StreamIt!',
+           version = '0.1',
+           author = 'Asier Santos',
            capabilities = {} 
          }
 end
 
 local translation = {
   int_search_devices = 'Search Devices',
-  int_help = 'Help',
   int_message = 'Message',
-  int_about = 'About',
-  int_selectedinfo = [[
-    <b><u>Name:</u></b>
-    <br>
-    <b><u>IP:</u></b>
-    <br>
-    <b><u>Vendor:</u></b>
-    <br>
-    <b><u>Others :</u></b> Info about the selected renderer
-    ]],
+  int_help = 'Help',
+  int_helpText = [[
+    <b><u>Lorem ipsum:</u></b> <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna 
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit 
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+    occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum.]],
+  int_about = 'About', --TODO: redactar texto
+  int_aboutText = [[
+    <b><u>Lorem ipsum:</u></b> <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna 
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit 
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+    occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum.]], --TODO: redactar texto
   int_streamit = 'Stream It!',
   int_close = 'Close',
   int_renderer_name_dumb = 'render_name_default',
   int_fileInfo = [[
-    <b><u>Name:</u></b> Blah meh blah
-    <br>
-    <b><u>File location:</u></b> Blah meh blah
-    <br>
-    <b><u>Bibrarte:</u></b> Blah meh blah
-    <br>
-    <b><u>Resolution :</u></b> Blah meh blah
-    ]] ,
+    <b><u>Name:</u></b> Blah meh blah <br>
+    <b><u>File location:</u></b> Blah meh blah <br>
+    <b><u>Bibrarte:</u></b> Blah meh blah <br>
+    <b><u>Resolution :</u></b> Blah meh blah  ]],
   int_volumeUp = 'V+',
   int_volumeDown =  'V-',
   int_rendererInfo = [[
-    <b><u>Name:</u></b>
-    <br>
-    <b><u>IP:</u></b>
-    <br>
-    <b><u>Vendor:</u></b>
-    <br>
-    <b><u>Others :</u></b> Info about the selected renderer
-    ]],
+    <b><u>Name:</u></b> Blah meh blah <br>
+    <b><u>IP:</u></b> Blah meh blah <br>
+    <b><u>Vendor:</u></b> Blah meh blah <br>
+    <b><u>Others :</u></b> Blah meh blah ]],
   int_play = 'Play',
   int_pause = 'Pause',
   int_stop = 'Stop',
   int_status = 'Reproduction status',
+  int_back = 'Go back',
+  int_searching = 'Searching...',
 }
 
 local dlg = nil
+local input_table = {} -- General widget id reference
 
 function activate()    --Initialization
-    vlc.msg.dbg("StreamIt init") --Debug message
-    launch_main_menu()
+  vlc.msg.dbg('StreamIt init') --Debug message
+  launch_main_menu()
 end
 
 
 function close()
-    vlc.msg.dbg("StreamIt closing") --Debug message
-    vlc.deactivate()
+  vlc.msg.dbg('StreamIt closing') --Debug message
+  dlg = nil
+  collectgarbage() --important
+  vlc.deactivate()
 end
 
 
 function close_dlg() --Close a dialog so another one can open
-  vlc.msg.dbg("StreamIt closing dialog") --Debug message
+  vlc.msg.dbg('StreamIt closing dialog') --Debug message
 
   if dlg ~= nil then 
     dlg:hide() 
   end
   
   dlg = nil
-  collectgarbage() --important 
+  collectgarbage() --important
 end
 
 
 function launch_main_menu()
   close_dlg()
-  dlg = vlc.dialog("Stream It!")
-  list_devices = dlg:add_list(1, 1, 4, 4) 
+  dlg = vlc.dialog('Stream It!')
+  input_table['list_devices'] = dlg:add_list(1, 1, 4, 4) 
 
-  button_search = dlg:add_button(translation["int_search_devices"], search_devices, 5, 1, 2, 1)
-  button_help = dlg:add_button(translation["int_help"], dumb, 8, 1, 1, 1)
+  input_table['button_search'] = dlg:add_button(translation['int_search_devices'], search_devices, 5, 1, 2, 1)
+  input_table['button_help'] = dlg:add_button(translation['int_help'], launch_help, 8, 1, 1, 1)
 
-  label_search = dlg:add_label(translation["int_message"], 5, 2, 2, 1)
-  button_about = dlg:add_button(translation["int_about"], dumb, 8, 2, 1, 1)
+  input_table['spin'] = dlg:add_spin_icon(5, 2, 1, 1)
+  input_table['label_search'] = dlg:add_label(translation['int_message'], 6, 2, 2, 1)
+  input_table['button_about'] = dlg:add_button(translation['int_about'], launch_about, 8, 2, 1, 1)
 
-  html_selectedinfo = dlg:add_html(translation["int_selectedinfo"], 5, 3, 2, 2) --TODO: extraerlo a un texto/funcion aparte para actualziarlo con facilidad
+  input_table['html_selectedinfo'] = dlg:add_html(translation['int_rendererInfo'], 5, 3, 2, 2) --TODO: extraerlo a un texto/funcion aparte para actualziarlo con facilidad
 
-  button_streamit = dlg:add_button(translation["int_streamit"], launch_control_menu, 1, 5, 4, 1)
-  button_close = dlg:add_button(translation["int_close"], close, 6, 5, 3, 1)
+  input_table['button_streamit'] = dlg:add_button(translation['int_streamit'], launch_control_menu, 1, 5, 4, 1)
+  input_table['button_close'] = dlg:add_button(translation['int_close'], close, 6, 5, 3, 1)
+
+  getFileInfo()
 
   dlg:show()
 end    
@@ -97,28 +108,61 @@ end
 
 function launch_control_menu()
   close_dlg()
-  dlg = vlc.dialog("Streaming to :"..translation["int_renderer_name_dumb"])
+  dlg = vlc.dialog('Streaming to :'..translation['int_renderer_name_dumb'])
   
-  html_fileInfo = dlg:add_html(translation["int_fileInfo"], 1, 1, 6, 4) --TODO: extraerlo a un texto/funcion aparte para actualziarlo con facilidad
+  input_table['html_fileInfo'] = dlg:add_html(translation['int_fileInfo'], 1, 1, 6, 4) --TODO: extraerlo a un texto/funcion aparte para actualziarlo con facilidad
 
-  button_volumeUp = dlg:add_button(translation["int_volumeUp"], dumb, 7, 1, 1, 1)
-  button_volumeDown = dlg:add_button(translation["int_volumeDown"], dumb, 7, 4, 1, 1)
+  input_table['button_volumeUp'] = dlg:add_button(translation['int_volumeUp'], dumb, 7, 1, 1, 1)
+  input_table['button_volumeDown'] = dlg:add_button(translation['int_volumeDown'], dumb, 7, 4, 1, 1)
 
-  html_rendererInfo = dlg:add_html(translation["int_rendererInfo"], 8, 1, 3, 4) --TODO: extraerlo a un texto/funcion aparte para actualziarlo con facilidad
+  input_table['html_rendererInfo'] = dlg:add_html(translation['int_rendererInfo'], 8, 1, 3, 4) --TODO: extraerlo a un texto/funcion aparte para actualziarlo con facilidad
   
-  button_play = dlg:add_button(translation["int_play"], dumb, 1, 5, 2, 1)
-  button_pause = dlg:add_button(translation["int_pause"], dumb, 3, 5, 2, 1)
-  button_stopp = dlg:add_button(translation["int_stop"], dumb, 5, 5, 2, 1)
-  button_close = dlg:add_button(translation["int_close"], close, 9, 5, 2, 1)
+  input_table['button_play'] = dlg:add_button(translation['int_play'], dumb, 1, 5, 2, 1)
+  input_table['button_pause'] = dlg:add_button(translation['int_pause'], dumb, 3, 5, 2, 1)
+  input_table['button_stop'] = dlg:add_button(translation['int_stop'], dumb, 5, 5, 2, 1)
+  input_table['button_back'] = dlg:add_button(translation['int_back'], launch_main_menu, 9, 5, 2, 1)
 
-  label_search = dlg:add_label(translation["int_status"], 1, 6, 10, 1)
+  input_table['label_search'] = dlg:add_label(translation['int_status'], 1, 6, 10, 1)
 
   dlg:show()
 end 
 
+function launch_about()
+  close_dlg()
+  dlg = vlc.dialog('Stream It! >> '..translation['int_help'])
+
+  input_table['html_rendererInfo'] = dlg:add_html(translation['int_helpText'], 1, 1, 8, 8)
+  input_table['button_back'] = dlg:add_button(translation['int_back'], launch_main_menu, 1, 9, 8, 1)
+  
+  dlg:show()
+end
+
+
+function launch_help()
+  close_dlg()
+  dlg = vlc.dialog('Stream It! >> '..translation['int_about'])
+
+  input_table['html_rendererInfo'] = dlg:add_html(translation['int_aboutText'], 1, 1, 8, 8)   
+  input_table['button_back'] = dlg:add_button(translation['int_back'], launch_main_menu, 1, 9, 8, 1)
+
+  dlg:show()
+end
+
 
 function search_devices() --Search devices on the LAN   
     --TODO: develop this 
+  services=vlc.sd.get_services_names()
+  input_table['label_search']:set_text(translation['int_searching'])
+
+  input_table['spin']:animate()
+
+  input_table['list_devices']:clear()
+
+  for i=1,5,1 --TODO: add real renderers' info
+  do 
+    input_table['list_devices']:add_value("Item"..i, 1) 
+  end
+
 end
 
 
@@ -126,7 +170,15 @@ function dumb()
     --TODO: delete this function
 end
 
-
+function getFileInfo ()
+  
+  if vlc.input.item() == nil then 
+    text='No file'
+  else
+    text = vlc.input.item():name()
+  end
+  input_table['label_search']:set_text(text)
+end
 
 
 
@@ -140,6 +192,67 @@ d:add_check_box( text, state, ... )
 d:add_dropdown( ... )
 d:add_list( ... ):        Allows multiple or empty selections.
 d:add_image( path, ... )
+
+Input
+-----
+input.is_playing(): Return true if input exists.
+input.add_subtitle(url): Add a subtitle file (by path) to the current input
+input.item(): Get the current input item. Input item methods are:
+  :uri(): Get item's URI.
+  :name(): Get item's name.
+  :duration(): Get item's duration in seconds or negative value if unavailable.
+  :is_preparsed(): Return true if meta data has been preparsed
+  :metas(): Get meta data as a table.
+  :set_meta(key, value): Set meta data.
+  :info(): Get the current input's info. Return value is a table of tables. Keys of the top level table are info category labels.
+  :stats(): Get statistics about the input. This is a table with the following fields:
+    .read_packets
+    .read_bytes
+    .input_bitrate
+    .average_input_bitrate
+    .demux_read_packets
+    .demux_read_bytes
+    .demux_bitrate
+    .average_demux_bitrate
+    .demux_corrupted
+    .demux_discontinuity
+    .decoded_audio
+    .decoded_video
+    .displayed_pictures
+    .lost_pictures
+    .sent_packets
+    .sent_bytes
+    .send_bitrate
+    .played_abuffers
+    .lost_abuffers
+
+
+    OSD
+---
+osd.icon( type, [id] ): Display an icon on the given OSD channel. Uses the
+  default channel is none is given. Icon types are: 'pause', 'play',
+  'speaker' and 'mute'.
+osd.message( string, [id], [position], [duration] ): Display the text message on
+  the given OSD channel. Position types are: 'center', 'left', 'right', 'top',
+  'bottom', 'top-left', 'top-right', 'bottom-left' or 'bottom-right'. The
+  duration is set in microseconds.
+osd.slider( position, type, [id] ): Display slider. Position is an integer
+  from 0 to 100. Type can be 'horizontal' or 'vertical'.
+osd.channel_register(): Register a new OSD channel. Returns the channel id.
+osd.channel_clear( id ): Clear OSD channel.
+
+Services discovery
+------------------
+
+Interfaces and extensions can use the following SD functions:
+
+sd.get_services_names(): Get a table of all available service discovery
+  modules. The module name is used as key, the long name is used as value.
+sd.add( name ): Add service discovery.
+sd.remove( name ): Remove service discovery.
+sd.is_loaded( name ): Check if service discovery is loaded.
+...
+
 --]]
 
 --    var.libvlc_command( name, argument ): 
@@ -147,10 +260,10 @@ d:add_image( path, ... )
 
 
 
- --[[
+ 
 function deactivate() --TODO: Useful??
-  vlc.msg.dbg("StreamIt closing") --Debug message
+  vlc.msg.dbg('StreamIt closing') --Debug message
   if dlg then
     dlg:hide() 
   end
-end]]
+end
